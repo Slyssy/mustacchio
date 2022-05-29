91046ee059b8a8292692984dffeed8d52ac3e9f9
Stephen <stephen.lyssy@gmail.com>
2022-05-28 20:48:24 -0500

    Added a modal so that when a gallery image is clicked, the modal opens.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,8 +1,29 @@
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 
+const thumbNail = document.querySelectorAll('.image-links');
+const modalContainer = document.querySelector('#modal-container');
+const close = document.querySelector('.close-modal');
+const modalImage = document.querySelector('.modal-image');
+
 //% Navbar toggle button event listener
 toggleButton.addEventListener('click', (e) => {
   e.preventDefault();
   navbarLinks.classList.toggle('active');
+});
+
+//% Function to select Image for Modal
+function selectModalImage(num) {
+  const thumb = `../images/gallery/gallery-image-${num}.jpg`;
+  modalImage.src = thumb;
+}
+
+thumbNail.forEach((image) => {
+  image.addEventListener('click', (event) => {
+    modalContainer.classList.add('show');
+  });
+});
+
+close.addEventListener('click', (event) => {
+  modalContainer.classList.remove('show');
 });
